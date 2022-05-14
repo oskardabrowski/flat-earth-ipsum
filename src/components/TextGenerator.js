da573e0736d84dd504b96189349b99d0232d987e
Oskar Dabrowski <oskar.dabrowski12890@wp.pl>
2022-05-14 12:57:07 +0200

    Adding first functions and fix styles

--- src/components/TextGenerator.js
+++ src/components/TextGenerator.js
@@ -1,43 +1,94 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import styled from "styled-components";
-import FEIbackground from "../img/FEIBackground.png";
 import FEIlogo from "../img/FEIlogo.svg";
 import { FiArrowUp, FiArrowDown } from "react-icons/fi";
 
 const TextGenerator = () => {
+  const [paragraphs, setParagraphs] = useState(1);
+  const [textArr, setTextArr] = useState([]);
+  const paragraphsRef = useRef(null);
+  const typeRef = useRef(null);
+  const checkboxRef = useRef(null);
+
+  const increment = (e) => {
+    e.preventDefault();
+    const inc = paragraphs + 1;
+    setParagraphs(inc);
+  };
+  const decrement = (e) => {
+    e.preventDefault();
+    if (paragraphs > 1) {
+      const dec = paragraphs - 1;
+      setParagraphs(dec);
+    }
+  };
+
+  const generateText = (e) => {
+    e.preventDefault();
+    const numOfParagraphs = paragraphsRef.current.value;
+    const type = typeRef.current.value;
+    const startFrom = checkboxRef.current.checked;
+    let starterUsed = false;
+
+    const paragraphsArr = [];
+
+    if (startFrom && !starterUsed) {
+      let paragraph = "";
+      paragraph += "NASA kłamie... ";
+      const paragraphLength = [3, 6, 9];
+      const randomParagraph =
+        paragraphLength[Math.floor(Math.random(1, 5)) - 1];
+    } else {
+      let paragraph = "";
+      const paragraphLength = [3, 6, 9];
+      const randomParagraph =
+        paragraphLength[Math.floor(Math.random(1, 5)) - 1];
+    }
+
+    console.log(
+      `Paragraphs: ${numOfParagraphs}, Type: ${type}, StartFrom: ${startFrom}`
+    );
+  };
   return (
     <Generator>
-      <img src={FEIbackground} alt={FEIbackground} />
       <div className="AppHead">
         <div className="AppHead-logo">
           <img src={FEIlogo} alt={FEIlogo} />
         </div>
         <div className="AppHead-form">
-          <form className="AppHead-form-options">
+          <form
+            className="AppHead-form-options"
+            onSubmit={(e) => generateText(e)}
+          >
             <div className="AppHead-form-options-form">
               <div className="AppHead-form-options-form-paragraphs">
-                <label for="numberOfParagraphs">Ilość paragrafów:</label>
+                <label htmlFor="numberOfParagraphs">Ilość paragrafów:</label>
                 <div className="AppHead-form-options-form-paragraphs-num">
-                  <input id="numberOfParagraphs" type="number" value="1" />
+                  <input
+                    id="numberOfParagraphs"
+                    type="number"
+                    value={paragraphs}
+                    ref={paragraphsRef}
+                  />
                   <div className="AppHead-form-options-form-paragraphs-num-arrows">
-                    <button>
+                    <button onClick={(e) => increment(e)}>
                       <FiArrowUp />
                     </button>
-                    <button>
+                    <button onClick={(e) => decrement(e)}>
                       <FiArrowDown />
                     </button>
                   </div>
                 </div>
               </div>
               <div className="AppHead-form-options-form-types">
-                <label for="typeSelection">Wybierz typ: </label>
-                <select id="typeSelection">
-                  <option>Mieszany</option>
-                  <option>Pseudonaukowy</option>
-                  <option>Pseudoreligijny</option>
+                <label htmlFor="typeSelection">Wybierz typ: </label>
+                <select id="typeSelection" ref={typeRef}>
+                  <option value="mixed">Mieszany</option>
+                  <option value="science">Pseudonaukowy</option>
+                  <option value="faith">Pseudoreligijny</option>
                 </select>
                 <span>
-                  <input type="checkbox" />
+                  <input type="checkbox" ref={checkboxRef} />
                   Zacznij od "NASA kłamie... "
                 </span>
               </div>
@@ -49,35 +100,38 @@ const TextGenerator = () => {
         </div>
       </div>
       <div className="GeneratedText">
-        <div>
-          <p>
-            Cow queso cheesy feet. Macaroni cheese cow cheesy feet feta
-            bocconcini red leicester melted cheese pecorino. Fondue feta cheese
-            on toast say cheese say cheese edam bocconcini cheesy feet. Blue
-            castello who moved my cheese roquefort. Danish fontina chalk and
-            cheese airedale. Boursin st. agur blue cheese cheese slices cheese
-            and biscuits bocconcini smelly cheese manchego halloumi. When the
-            cheese comes out everybody's happy lancashire squirty cheese ricotta
-            mozzarella rubber cheese the big cheese lancashire. Bavarian
-            bergkase monterey jack hard cheese ricotta cheese on toast bavarian
-            bergkase mascarpone parmesan. Cheesy feet stilton melted cheese.
-            Cheese triangles brie cheesy feet. Croque monsieur caerphilly paneer
-            cheesecake taleggio airedale fondue feta. Cheeseburger who moved my
-            cheese melted cheese roquefort caerphilly cheese on toast goat
-            camembert de normandie. Manchego melted cheese cheese and biscuits
-            cauliflower cheese airedale squirty cheese boursin feta. Macaroni
-            cheese caerphilly. Cheesecake ricotta ricotta. Chalk and cheese
-            roquefort stilton fromage fromage frais lancashire cheesy feet
-            pepper jack. Macaroni cheese the big cheese dolcelatte cheddar who
-            moved my cheese monterey jack stinking bishop bavarian bergkase.
-            Rubber cheese hard cheese cheesy feet cut the cheese parmesan
-            squirty cheese. Squirty cheese stilton fromage. Taleggio who moved
-            my cheese brie boursin cauliflower cheese stilton boursin jarlsberg.
-            Danish fontina queso bavarian bergkase manchego queso feta queso
-            cheese and wine. Mascarpone goat swiss stilton red leicester goat
-            emmental swiss. Macaroni cheese blue castello stilton jarlsberg.
-          </p>
-        </div>
+        {textArr.length > 0 && (
+          <div>
+            <p>
+              Cow queso cheesy feet. Macaroni cheese cow cheesy feet feta
+              bocconcini red leicester melted cheese pecorino. Fondue feta
+              cheese on toast say cheese say cheese edam bocconcini cheesy feet.
+              Blue castello who moved my cheese roquefort. Danish fontina chalk
+              and cheese airedale. Boursin st. agur blue cheese cheese slices
+              cheese and biscuits bocconcini smelly cheese manchego halloumi.
+              When the cheese comes out everybody's happy lancashire squirty
+              cheese ricotta mozzarella rubber cheese the big cheese lancashire.
+              Bavarian bergkase monterey jack hard cheese ricotta cheese on
+              toast bavarian bergkase mascarpone parmesan. Cheesy feet stilton
+              melted cheese. Cheese triangles brie cheesy feet. Croque monsieur
+              caerphilly paneer cheesecake taleggio airedale fondue feta.
+              Cheeseburger who moved my cheese melted cheese roquefort
+              caerphilly cheese on toast goat camembert de normandie. Manchego
+              melted cheese cheese and biscuits cauliflower cheese airedale
+              squirty cheese boursin feta. Macaroni cheese caerphilly.
+              Cheesecake ricotta ricotta. Chalk and cheese roquefort stilton
+              fromage fromage frais lancashire cheesy feet pepper jack. Macaroni
+              cheese the big cheese dolcelatte cheddar who moved my cheese
+              monterey jack stinking bishop bavarian bergkase. Rubber cheese
+              hard cheese cheesy feet cut the cheese parmesan squirty cheese.
+              Squirty cheese stilton fromage. Taleggio who moved my cheese brie
+              boursin cauliflower cheese stilton boursin jarlsberg. Danish
+              fontina queso bavarian bergkase manchego queso feta queso cheese
+              and wine. Mascarpone goat swiss stilton red leicester goat
+              emmental swiss. Macaroni cheese blue castello stilton jarlsberg.
+            </p>
+          </div>
+        )}
       </div>
       <footer className="Footer">
         <div>Copyright &copy; Oskar Dąbrowski {new Date().getFullYear()}</div>
@@ -90,43 +144,42 @@ const Generator = styled.div`
   width: 100%;
   height: max-content;
   position: relative;
+  background-image: url("./FEIBackground.png");
+  background-size: cover;
+  background-repeat: no-repeat;
+  height: 500vh;
 
   .Footer {
-    position: absolute;
-    top: 500vh;
     background-color: white;
     z-index: 100;
     width: 100%;
     display: flex;
     align-items: center;
     justify-content: center;
+    flex-direction: column;
+    height: 25vh;
     & > div {
       padding: 1rem;
       font-family: "Roboto Condensed";
+      font-size: 1.2rem;
+      font-weight: bold;
     }
   }
 
-  & > img {
-    width: 100%;
-    position: absolute;
-    top: 0;
-    left: 0;
-  }
-
   .GeneratedText {
-    top: 105vh;
-    position: absolute;
-    background-color: white;
-    width: 85%;
-    left: 50%;
-    max-height: 350vh;
-    transform: translate(-50%, 0%);
-    padding: 2rem;
-    border-radius: 15px;
-    text-align: justify;
-    overflow-y: scroll;
+    position: relative;
+    height: 350vh;
+
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    border-radius: 0px;
+
     font-family: "Roboto Condensed";
     font-size: 1.5rem;
+    margin-bottom: 25vh;
     ::-webkit-scrollbar {
       width: 10px;
     }
@@ -140,6 +193,14 @@ const Generator = styled.div`
     ::-webkit-scrollbar-thumb:hover {
       background: blue;
     }
+    & > div {
+      width: 85vw;
+      height: 350vh;
+      padding: 2rem;
+      text-align: justify;
+      overflow-y: scroll;
+      background-color: white;
+    }
     & > div > p {
       margin-bottom: 2rem;
       line-height: 2.5rem;
@@ -148,7 +209,7 @@ const Generator = styled.div`
 
   .AppHead {
     z-index: 100;
-    position: absolute;
+    position: relative;
     width: 100%;
     height: 100vh;
     display: flex;
---
+++ src/components/database.js
@@ -0,0 +1,8 @@
+export const religion = [
+  "Ziemia jest dyskiem z kopułą",
+  "Jesteśmy w centrum wszechświata",
+];
+export const science = [
+  "Ziemia jest dyskiem z kopułą",
+  "Grawitacja nie istnieje a nas do ziemi przyciąga elektromagnetyzm",
+];
